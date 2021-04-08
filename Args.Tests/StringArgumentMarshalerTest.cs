fea9c6287c7cc6776ed08d9f9c6088bccc806a14
Robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-04-08 16:57:47 +0800

    Refactor StringArgumentMarshaler Test

---
+++ Args.Tests/StringArgumentMarshalerTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Args.Exceptions;
+using Args.Marshalers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Args.Tests
+{
+    [TestFixture]
+    public class StringArgumentMarshalerTest
+    {
+        private IEnumerator<string> enumerable;
+        private StringArgumentMarshaler stringArgumentMarshaler;
+
+        [SetUp]
+        public void Setup()
+        {
+            stringArgumentMarshaler = new StringArgumentMarshaler();
+            enumerable = Substitute.For<IEnumerator<string>>();
+        }
+
+        [Test]
+        public void WhenInitializedString()
+        {
+            GivenSingleCurrentArgument();
+
+            ShouldeBeEqualErrorCode(ErrorCodes.MISSING_STRING);
+        }
+
+        private void ShouldeBeEqualErrorCode(ErrorCodes errorCode)
+        {
+            Assert.AreEqual(errorCode,
+                Assert.Throws<ArgsException>(() => stringArgumentMarshaler.Set(enumerable)).GetErrorCode());
+        }
+
+        private void GivenSingleCurrentArgument()
+        {
+            enumerable.Current.Returns((string) null);
+            enumerable.MoveNext().Returns(false);
+        }
+    }
+}
